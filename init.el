16e1edca9d3c218e510d4441af6a51c1f278f55d
jelaev <jelaev@yandex.ru>
2024-01-06 20:50:11 +0300

    Use-package

--- init.el
+++ init.el
@@ -4,16 +4,28 @@
 ;; and `package-pinned-packages`. Most users will not need or want to do this.
 ;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+(use-package use-package
+  :ensure t)
+
+(use-package company
+  :ensure t)
+
+(use-package clojure-mode
+  :ensure t)
+
+(use-package cider
+  :ensure t)
+
+(use-package projectile
+  :ensure t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(whiteboard))
  '(inferior-lisp-program "clojure")
- '(package-selected-packages
-   '(projectile cider clojure-mode company use-package)))
+ '(package-selected-packages '(projectile cider clojure-mode company use-package)))
 
 ;; Move backup files
 (setq backup-directory-alist `(("." . "~/.saves_emacs")))
