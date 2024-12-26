a336c57ecf381dedd3e1d6f58cb4ebe0b5bd5670
jelaev <jelaev@users.noreply.github.com>
2024-12-26 09:42:13 +0300

    Update init.el

--- init.el
+++ init.el
@@ -1,14 +1,17 @@
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; and `package-pinned-packages`. Most users will not need or want to do this.
-;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
 (package-initialize)
+
 (use-package use-package
   :ensure t)
 
-(use-package company
+(use-package pbcopy
   :ensure t)
+
+(turn-on-pbcopy)
+
+(add-hook 'after-init-hook 'global-company-mode)
 
 (use-package clojure-mode
   :ensure t)
@@ -25,27 +28,26 @@
 (use-package cider
   :ensure t)
 
-(use-package vertico
-  :ensure t
-  :init
-  (vertico-mode))
-
 (use-package projectile
   :ensure t
   :init
   (projectile-mode +1)
   :bind (:map projectile-mode-map
-	      ("C-c p" . projectile-command-map))
-  )
+	      ("C-c p" . projectile-command-map)))
+
+(use-package company
+  :ensure t)
+
+(use-package vertico
+  :ensure t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(inferior-lisp-program "clojure")
  '(package-selected-packages
-   '(clojure-lsp vertico projectile cider clojure-mode company use-package)))
+   '(pbcopy all-the-icons dashboard nerd-icons page-break-lines projectile cider flycheck lsp-treemacs lsp-mode clojure-mode company vertico-mode)))
 
 (add-hook 'prog-mode-hook 'display-line-numbers-mode)
 
@@ -57,12 +59,14 @@
 (add-hook 'clojurescript-mode-hook 'lsp)
 (add-hook 'clojure-mode-hook 'lsp)
 
-(setq company-idle-delay nil) ; never start completions automatically
-(global-set-key (kbd "C-<return>") #'company-complete) ; use M-TAB, a.k.a. C-M-i, as manual trigger
+(setq company-idle-delay 0)
+(setq company-dbabrev-downcase 0)
 
-;; Projectile dir
-(setq projectile-project-search-path '("~/projects"))
-(global-set-key (kbd "C-c p") #'projectile-command-map)  
+(global-set-key (kbd "C-c C-RET") #'company-complete)
+						    
+;;Projectile dir
+(setq projectile-project-search-path '("~/Projects/"))
+(global-set-key (kbd "C-c p") #'projctile-command-map)
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -70,3 +74,33 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;; KBD
+(global-set-key (kbd "C-x C-d") 'cider-find-var)
+(global-set-key (kbd "C-x .") 'cider-pop-back)
+
+(setq cider-use-xref t)
+(global-set-key (kbd "C-x C-r") 'xref-find-reference)
+
+(add-hook 'clojure-mode-hook #'imenu-add-menubar-index)
+(global-set-key (kbd "<C-i>") 'helm-imenu)
+(setq imenu-use-popup-menu nil)
+
+;; Emacs Awesome
+;; Dashboard
+
+(use-package page-break-lines
+  :ensure t)
+
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+(use-package nerd-icons)
+
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook))
+
+(setq dashboard-banner-logo-title "Alexandr Zhelaev dashboard")
